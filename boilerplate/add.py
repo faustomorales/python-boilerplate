253b4cafa36173a9ee4d7abb47b3d82186c3fe8b
Fausto Morales <faustomorales@gmail.com>
2019-12-07 12:29:41 -0600

    Add math representation.

--- boilerplate/add.py
+++ boilerplate/add.py
@@ -1,6 +1,10 @@
 def add(first_number: float, second_number: float) -> float:
     """This function adds two numbers together.
 
+    .. math::
+
+        a + b = s
+
     Args:
         first_number: The first number
         second_number: The second number
